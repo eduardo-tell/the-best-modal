f4c0e2464d2f93d365e0babd3e957e2aae6a2835
Eduardo Tell <eduardo.tell@dotgroup.com.br>
2020-05-24 20:25:07 -0300

    Inclui material design

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,50 @@
-var gulp        = require('gulp');
-var browserSync = require('browser-sync').create();
-var reload      = browserSync.reload;
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var stripDebug = require('gulp-strip-debug');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+const sync = require("browser-sync").create();
 
-gulp.task('serve', function () {
-    browserSync.init({
+gulp.task('default', ['watch'], function () { });
+
+gulp.task('sass', function () {
+    return gulp.src('css/sass/main.scss')
+        .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+        .pipe(concat('styles.min.css'))
+        .pipe(gulp.dest('css/dist'));
+});
+
+gulp.task('scripts', function () {
+
+    arquivos = [
+        'js/src/animate.min.js',
+        'js/src/cash.js',
+        'js/src/buttons.js',
+        'js/src/cards.js',
+        'js/src/carousel.js',
+        'js/src/chips.js',
+        'js/src/collapsible.js',
+        'js/src/component.js',
+        'js/src/datepicker.js',
+        'js/src/dropdown.js'
+    ]
+
+    return gulp.src(arquivos)
+        .pipe(stripDebug()).on('error', console.error)
+        .pipe(babel({ "presets": ['env'] })).on('error', console.error)
+        .pipe(concat('scripts.min.js')).on('error', console.error)
+        // .pipe(uglify()).on('error', console.error)
+        .pipe(gulp.dest('js/dist')).on('error', console.error)
+});
+
+gulp.task('watch', function () {
+    sync.init({
         server: {
             baseDir: "./"
         }
     });
-
-    gulp.watch("*.html").on("change", reload);
+    gulp.watch('css/sass/components/*.scss', ['sass']);
+    gulp.watch('js/src/*.js', ['scripts']);
 });
